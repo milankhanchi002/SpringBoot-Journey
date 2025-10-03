91776513c9f945a27b942397a6e79a41149e6d79
milankhanchi002 <milankhanchi@gmail.com>
2025-10-04 02:04:45 +0530

    adding transactional annotation

--- Hospital-Management/src/main/java/com/example/Hospital/Management/Service/PatientService.java
+++ Hospital-Management/src/main/java/com/example/Hospital/Management/Service/PatientService.java
@@ -3,6 +3,7 @@ package com.example.Hospital.Management.Service;
 
 import com.example.Hospital.Management.Repository.PatientRepository;
 import com.example.Hospital.Management.entity.Patient;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,14 @@ public class PatientService {
     @Autowired
     private PatientRepository patientrepo;
 
+    @Transactional
     public Patient getPatientById(Long id){
         Patient p1= patientrepo.findById(id).orElseThrow();
+        Patient p2=patientrepo.findById(id).orElseThrow();
+        System.out.println(p1==p2);
+
+        //here we dont need to save with .save method
+        p1.setName("yoyo");
         return p1;
     }
 }
--- Hospital-Management/src/main/java/com/example/Hospital/Management/entity/Patient.java
+++ Hospital-Management/src/main/java/com/example/Hospital/Management/entity/Patient.java
@@ -1,23 +1,27 @@
 package com.example.Hospital.Management.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDate;
 
 @Entity
 @ToString
+@Getter
+@Setter
 public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ToString.Exclude
+    @Column(name="patient_name",nullable = false,length=40)
     private String name;
+
     private LocalDate Birth_date;
+    @Column(unique = true,nullable = false)
     private String email;
     private String gender;
 //
