5b96fa8ede8e6207c01f070d6bc78ccb49a2f2be
milankhanchi002 <milankhanchi@gmail.com>
2025-10-08 23:21:39 +0530

    jpa methods

--- Hospital-Management/src/main/java/com/example/Hospital/Management/Repository/PatientRepository.java
+++ Hospital-Management/src/main/java/com/example/Hospital/Management/Repository/PatientRepository.java
@@ -4,7 +4,12 @@ package com.example.Hospital.Management.Repository;
 import com.example.Hospital.Management.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+
+import java.util.List;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient,Long> {
+    Patient findByName(String name);
+    List<Patient> findByBirthDateOrEmail(LocalDate birthDate, String email);
 }
